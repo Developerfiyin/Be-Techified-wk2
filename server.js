39e1196ff1520c699ec43dba0e52d1b5e764e1a3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-02 22:23:00 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  
  const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.use(express.json()); // ADDED EXPRESS MIDDLEWARE WHICH IS EXPRESS.JSON
 
