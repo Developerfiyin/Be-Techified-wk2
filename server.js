b7e3694b4dc28310ac34766343f80f5738856fca
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-02 22:10:56 +0100

    Created request body

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const port = 3000
 app.use(express.json()); // ADDED EXPRESS MIDDLEWARE WHICH IS EXPRESS.JSON
 
 app.post('/echo', (req, res) => {
-  res.json({echoed: req.body });  // REQ.BODY IS NOW AVAILABLE
+  console.log("req.body ")
+  res.json({echoed: req.body });   // REQ.BODY IS NOW AVAILABLE
 });
 app.get('/', (req, res) => {
   res.send('Hi I am present, my name is fiyinfoluwa!')
